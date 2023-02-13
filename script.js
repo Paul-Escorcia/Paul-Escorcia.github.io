c10667da2deb15442687f114b9506f53c4b98481
Paul Escorcia <paul.escorcia@gmail.com>
2023-02-12 23:03:27 -0700

    Added Fixed Social Bar

---
+++ script.js
@@ -0,0 +1,16 @@
+window.console = window.console || function(t) {};
+
+if (document.location.search.match(/type=embed/gi)) {
+    window.parent.postMessage("resize", "*");
+}
+
+$(document).ready(function () {
+  $('#so-close').click(function () {
+    $('.s-soft').addClass('so-collapse');
+    $('#so-open').delay(300).css('left', '0');
+});
+
+$('#so-open').click(function () {
+    $('#so-open').css('left', '-60px');
+    $('.s-soft').removeClass('so-collapse');
+});}); 
